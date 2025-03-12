ffb227f1ce07d46097a90aaef32f67323015c437
topduh <sanblackx1@gmail.com>
2025-03-11 19:15:37 -0500

    NEW: style change with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import "./App.css";
-import ListTextToPhpArray from "./components/ListConvert";
+import './App.css';
+import ListTextToPhpArray from './components/ListConvert';
 
 function App() {
   return (
-    <>
+    <main className=''>
+      {/* <h1 className='text-xl font-bold text-gray-900 dark:text-gray-100'>Expansion</h1> */}
       <ListTextToPhpArray />
-    </>
+    </main>
   );
 }
 
